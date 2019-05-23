105a4933d8d3078587a008fe825ef060fb643ba5
Максим Гаранин <floorup@outlook.com>
2019-05-23 19:24:27 +0400

    +базовое наполнение

---
+++ infokiosk.Core/Models/ServOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace infokiosk.Core.Models
+{
+
+    public class ServOrder
+    {
+        public long ServID { get; set; }
+        public string ServName { get; set; }
+        public string ServOGV { get; set; }
+        public string ServTime { get; set; }
+        public string ServLevel { get; set; }
+    }
+}
---
+++ infokiosk/Views/Page05Page.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using infokiosk.Core.Models;
+using infokiosk.Core.Services;
+
+using Windows.UI.Xaml.Controls;
+
+namespace infokiosk.Views
+{
+    public sealed partial class Page05Page : Page, INotifyPropertyChanged
+    {
+        // TODO WTS: Change the grid as appropriate to your app, adjust the column definitions on DataGridPage.xaml.
+        // For more details see the documentation at https://docs.microsoft.com/windows/communitytoolkit/controls/datagrid
+        public Page05Page()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<ServOrder> Source
+        {
+            get
+            {
+                // TODO WTS: Replace this with your actual data
+                return ServDataService.GetGridServData();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
